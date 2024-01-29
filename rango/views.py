a3fe4776bc1eb358a2ecae9641b5ec392a72419a
mherreradsci <marcelo.herrerat@gmail.com>
2024-01-29 00:33:40 -0300

    tango - cap 08 - '8.4 The render() Method and the request Contex' done.

--- rango/views.py
+++ rango/views.py
@@ -33,6 +33,11 @@ def index(request):
 # This view is simple (don't interact with models)
 def about(request):
     context_dict = {"username": "mherrera"}
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+
     return render(request, "rango/about.html", context=context_dict)
 
 
