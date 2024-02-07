67935605d4f83059b2d5da70b8c30f4867c87027
mherreradsci <marcelo.herrerat@gmail.com>
2024-02-07 00:25:05 -0300

    tango - cap 09 - Until cap '9.4 The User Model'

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 import re
 
 from django import forms
-
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 from rango.models import Category, Page
 
 
@@ -54,3 +55,14 @@ class PageForm(forms.ModelForm):
         exclude = ("category",)
         # or specify the fields to include (don't include the category field).
         # fields = ('title', 'url', 'views')
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
